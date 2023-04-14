07a18e617af30369acaf44f984fbdb3acc0b0f1f
Arsam <khmapa833@gmail.com>
2023-04-14 12:19:23 +0330

    firsttt

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //test for github
+        System.out.println("I'm in githubbbb");
+    }
+}
